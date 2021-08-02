2ea33586e0d1c3763227ae58f5b6594eadb81367
liuhaiweicheer <514896130@qq.com>
2021-08-03 00:43:11 +0800

    config

---
+++ cloud-config-center3344/src/main/java/com/demo/springcloud/MainAppConfigCenter3344.java
@@ -0,0 +1,18 @@
+package com.demo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: Liu Haiwei
+ * @Date: 2021/8/2 23:13
+ * @Description:
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class MainAppConfigCenter3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainAppConfigCenter3344.class, args);
+    }
+}
